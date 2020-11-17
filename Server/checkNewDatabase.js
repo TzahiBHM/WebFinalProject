31b1f802350cfa2ccb09a28050ad0c196cbc1803
TzahiBHM <48315181+TzahiBHM@users.noreply.github.com>
2020-11-17 10:55:48 +0200

    Update checkNewDatabase.js

--- Server/checkNewDatabase.js
+++ Server/checkNewDatabase.js
@@ -5,10 +5,10 @@ const mysql = require('mysql');
 
 
 let con = mysql.createConnection({
-    host: "us-cdbr-east-02.cleardb.com",
-    user: "b6a6991b1ab052",
-    password: "6f528f84",
-    database: "heroku_40a59a12660f2b2"
+    host: "",
+    user: "",
+    password: "",
+    database: ""
 });
 
 con.connect((err) => {
